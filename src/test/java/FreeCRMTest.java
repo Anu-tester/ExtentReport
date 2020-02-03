07afc9ec5ec1e298304e19e9579943d2f127e66c
Anu-tester <deshpandeanu17@gmail.com>
2020-02-02 18:38:23 -0800

    added newline multipipeline

--- src/test/java/FreeCRMTest.java
+++ src/test/java/FreeCRMTest.java
@@ -4,12 +4,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -57,7 +58,7 @@ public class FreeCRMTest {
 		String destination = System.getProperty("user.dir") + "/FailedTestsScreenshots/" + screenshotName + dateName
 				+ ".png";
 		File finalDestination = new File(destination);
-		FileUtils.copyFile(source, finalDestination);
+		FileHandler.copy(source, finalDestination);
 		return destination;
 	}
 	
